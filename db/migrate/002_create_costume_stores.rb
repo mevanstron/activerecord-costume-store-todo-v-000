de23a5b6457778041d8ccf492b02250fce98c7ca
Michael Evans <michael@michaelgregoryevans.com>
2017-11-22 16:05:38 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,8 @@
 # Create your costume_stores migration here
+class CreateCostumeStores
+  create_table do |t|
+    t.string :name
+    t.string :location
+    t.integer :costume_inventory
+    
+end
