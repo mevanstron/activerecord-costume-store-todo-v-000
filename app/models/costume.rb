7a440cc0b662128370d26a4cd21c7e4f8edb9c70
Michael Evans <michael@michaelgregoryevans.com>
2017-11-22 15:40:28 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,8 +1,13 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
 class Costume < ActiveRecord::Base
-  def created_at
-    Time.now.utc.to_datetime
+  attr_accessor :created_at
+  def initialize
+    self.created_at
+  end
+
+  def created_at=
+    @created_at = Time.now.utc.to_datetime
   end
 
   def updated_at
