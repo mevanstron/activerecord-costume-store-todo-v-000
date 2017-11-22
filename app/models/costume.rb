a7b89735738a366962876a7557f384d2137cb40d
Michael Evans <michael@michaelgregoryevans.com>
2017-11-22 15:47:39 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -3,11 +3,11 @@
 class Costume < ActiveRecord::Base
 
 
-  def created_at
-    @created_at = Time.now.utc.to_datetime
-  end
+  #def created_at
+    #@created_at = Time.now.utc.to_datetime
+  #end
 
-  def updated_at
-    Time.now.utc.to_datetime
-  end
+  #def updated_at
+    #Time.now.utc.to_datetime
+  #end
 end
